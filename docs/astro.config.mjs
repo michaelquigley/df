856be45cc2028933cf94362fcc096ba038dd9a84
Michael Quigley <michael@quigley.com>
2025-08-29 23:21:54 -0400

    base url? (#12)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	base: process.env.BASE_URL || '/df',
 	integrations: [
 		starlight({
 			title: 'df',
